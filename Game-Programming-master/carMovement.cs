c7e09cf17bba1ad6111aa8f48bad6466f7750524
DFerre13 <devinferre@gmail.com>
2020-04-16 05:25:21 -0700

    test

---
+++ Game-Programming-master/carMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carMovement : MonoBehaviour
+{
+    public float speed;
+    public float gravity;
+    public float jumpHeight;
+    private Vector3 direction;
+    private Vector3 walkingVelocity;
+    private Vector3 fallingVelocity;
+    private CharacterController controller;
+    private AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 8.0f;
+        gravity = 9.8f;
+        direction = Vector3.zero;
+        walkingVelocity = Vector3.zero;
+        fallingVelocity = Vector3.zero;
+        controller = GetComponent<CharacterController>();
+        audio = GetComponent<AudioSource>();
+        //audio.Play(); // car sound
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+        direction = direction.normalized;
+        if (direction != Vector3.zero)
+            transform.forward = direction;
+        walkingVelocity = direction * speed;
+        controller.Move(walkingVelocity * Time.deltaTime);
+
+        fallingVelocity.y -= gravity * Time.deltaTime;
+
+        while (Input.GetKeyDown(KeyCode.W))
+        {
+            audio.Play();
+        }
+
+        
+
+        /*
+        if (Input.GetButtonDown("Jump") && fallingVelocity.y <= -6)
+        {
+            audio.Play();
+            fallingVelocity.y = Mathf.Sqrt(gravity * jumpHeight);
+        }
+        */
+        controller.Move(fallingVelocity * Time.deltaTime);
+
+    }
+
+    // when the car hit an object 
+    void OnCollisionEnter(Collision obj)
+    {
+        if (obj.gameObject.name == "Rock")  // the name of the object Rock
+        {
+            Destroy(this.gameObject);  // To destroy the car
+            Destroy(obj.gameObject); // To destroy the object
+        }
+
+        if (obj.gameObject.name == "Coin")  // the name of the object Rock
+        {
+            Destroy(obj.gameObject); // destroy coin
+
+        }
+    }
+}
---
+++ Game-Programming-master/gamingProject-master/Assets/gainHealth.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gainHealth : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        healthBarScript.health += 20f;
+        Destroy (gameObject);
+    }
+}
