014d1b1f06e3a4cbd65e55f2c12510adfc33ee3a
juare066 <56847588+juare066@users.noreply.github.com>
2020-04-21 20:52:52 -0700

    Working Game

---
+++ gamingProject-master/Assets/CubeHealth.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeHealth : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider Cube)
+    {
+        healthBarScript.health -= 20f;
+        //Destroy(gameObject);
+    }
+}
