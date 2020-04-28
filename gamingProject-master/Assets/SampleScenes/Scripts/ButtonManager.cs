be6ba886a23bbe8946857733af7d664e991b34ef
DFerre13 <devinferre@gmail.com>
2020-04-28 15:53:06 -0700

    Test1

---
+++ gamingProject-master/Assets/SampleScenes/Scripts/ButtonManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void NewGameBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void Level1()
+    {
+        SceneManager.LoadScene(2); //Level1
+	}
+    public void Level2()
+    {
+        SceneManager.LoadScene(3); //Level2
+	}
+    public void Level3()
+    {
+        SceneManager.LoadScene(4); //Level3
+	}
+    public void backButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+}
