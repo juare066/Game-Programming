2d42e73dbd315374211a8559a14a67b53f7d5d09
juare066 <56847588+juare066@users.noreply.github.com>
2020-05-11 22:47:24 -0700

    Working Game

---
+++ gamingProject-master/Assets/SampleScenes/Scripts/healthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class healthBar : MonoBehaviour
+{
+
+    public int maxHealth = 5;
+    public int health;
+
+    public Slider slider;
+
+    // Start is called before the first frame update
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        //FindObjectOfType<GameManager>().loseGame();  
+    }
+}
---
+++ gamingProject-master/Assets/Standard Assets/ParticleSystems/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public int rotateSpeed = 1;
+    private void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+}
