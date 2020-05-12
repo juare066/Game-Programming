9c10596a3caf397ca7228870928947cd4a832fc8
yako004 <54650577+yako004@users.noreply.github.com>
2020-05-12 14:44:53 -0700

    Revert "Merge branch 'master' into devinsUpdate"
    This reverts commit 6e9b2bb03e852607cdb4b391fb7d66cc67f2dd76, reversing
    changes made to bbc356a7c410c4a95ab05a016beba4db4b3b6d6a.

--- gamingProject-master/Assets/SampleScenes/Scripts/gainHealth.cs
+++ gamingProject-master/Assets/SampleScenes/Scripts/gainHealth.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class gainHealth : MonoBehaviour
 {
-    private AudioSource collectedSound; // sound to be played when collecting co
-
     private void OnTriggerEnter(Collider other)
     {
-//        collectedSound.Play();
         FindObjectOfType<player>().GainHealth(1);
         ScoreScript.scoreValue += 20;
-        Destroy(gameObject);
-
+        Destroy (gameObject);
     }
 }
